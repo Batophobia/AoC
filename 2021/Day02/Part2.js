c87e53e63709730decbbd54d458c8a4c104b431f
Batophobia <godrox3@yahoo.com>
2021-12-02 12:07:51 -0600

    Update Part2.js

--- 2021/Day02/Part2.js
+++ 2021/Day02/Part2.js
@@ -1,5 +1,17 @@
 function aoc(){
   inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  inpt = inpt.split(/\n/g);
+  x = 0; y = 0; aim = 0;
+  for(i = 0; i < inpt.length; i++){
+    line = inpt[i].split(' ');
+    switch(line[0]){
+      case 'up': aim -= parseInt(line[1]); break;
+      case 'down': aim += parseInt(line[1]); break;
+      case 'forward':
+        x += parseInt(line[1]);
+        y += aim * parseInt(line[1]);
+        break;
+    }
+  }
+  return x * y;
 }

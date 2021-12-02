ce95ae7d636dffd9c514e11e4d01182e12d5a2ee
Batophobia <godrox3@yahoo.com>
2021-12-02 11:55:09 -0600

    Update Part2.js

--- 2021/Day01/Part2.js
+++ 2021/Day01/Part2.js
@@ -1,5 +1,13 @@
 function aoc(){
   inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  inpt = inpt.split(/\n/g).filter(v => v).map(v=>parseInt(v));
+  ttl = 0; prev = inpt[0] + inpt[1] + inpt[2];
+  for(i=1;++i<inpt.length;){
+    tmp = inpt[i-2] + inpt[i-1] + inpt[i];
+    if(tmp > prev){
+      ttl++;
+    }
+    prev = tmp;
+  }
+  return ttl;
 }

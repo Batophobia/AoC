58e5602902684339c2d72ad4f631dac754b1a027
Batophobia <godrox3@yahoo.com>
2021-12-06 09:22:30 -0600

    Update Part2.js

--- 2021/Day06/Part2.js
+++ 2021/Day06/Part2.js
@@ -1,5 +1,13 @@
 function aoc(){
   inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  inpt = inpt.split(/,/g).filter(v=>v).map(v=>parseInt(v));
+  fish = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0};
+  for(i = 0; i < inpt.length; i++) fish[inpt[i]]++;
+	for(d = 0; d < 256; d++){
+		tmp = fish[0];
+		for(i = 1; i < 9; i++) fish[i-1] = fish[i];
+		fish[8] = tmp;
+    fish[6] += tmp;
+  }
+	return Object.values(fish).reduce((t, n) => t + n);
 }

9ae953ffadec25b897209ab4c13ca28e05281df0
Batophobia <godrox3@yahoo.com>
2021-12-08 08:52:02 -0600

    Update Part1.js

--- 2021/Day08/Part1.js
+++ 2021/Day08/Part1.js
@@ -1,5 +1,17 @@
 function aoc(){
   inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  inpt = inpt.split(/\n/g).filter(v=>v);
+  ttl = 0;
+  for(i = 0; i < inpt.length; i++){
+    otpt = inpt[i].split(' | ')[1].split(' ');
+    for(o = 0; o < otpt.length; o++){
+      switch(otpt[o].length){
+        case 2:
+				case 3:
+				case 4:
+        case 7: ttl++; break;
+      }
+    }
+  }
+  return ttl;
 }

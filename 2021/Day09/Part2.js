e5d4b808e4262898757db45133b560a570a145b7
Batophobia <godrox3@yahoo.com>
2021-12-09 10:01:30 -0600

    Update Part2.js
    derp

--- 2021/Day09/Part2.js
+++ 2021/Day09/Part2.js
@@ -1,31 +1,34 @@
+inpt = [];
 function aoc(){
   inpt = document.body.textContent || document.body.innerText;
-inpt = '2199943210\n3987894921\n9856789892\n8767896789\n9899965678';
   inpt = inpt.split(/\n/g).filter(v=>v);
   sizes = [];
   for(y = 0; y < inpt.length; y++){
     inpt[y] = inpt[y].split('').map(n => parseInt(n));
+  }
+  for(y = 0; y < inpt.length; y++){
     for(x = 0; x < inpt[0].length; x++){
       if(inpt[y][x] > -1 && inpt[y][x] < 9)
-        [tmpArr, tmpSize] = flood(inpt, y, x);
-        inpt = tmpArr;
-        sizes.push(tmpSize);
+        sizes.push(flood(y, x));
     }
   }
-  sizes = sizes.sort().reverse();
+  sizes = sizes.sort((a,b) => b-a);
   return sizes[0] * sizes[1] * sizes[2];
 }
-function flood(arr, row, col){
-  return fill(arr, arr[row][col], row, col, 0);
+function flood(row, col){
+  return fill(row, col, 0);
 }
-function fill(arr, row, col, size){
-  if(row < 0 || col < 0 || row > arr.length-1 || col > arr[0].length-1) return [arr, size];
-  if(arr[row][col]==9 || arr[row][col]==-1) return [arr, size];
-  arr[row][col] = -1;
+function fill(row, col, size){
+  if(inpt[row][col]==9 || inpt[row][col]==-1) return size;
+  inpt[row][col] = -1;
   size++;
-  [arr, size] = fill(arr, row+1, col, size);
-  [arr, size] = fill(arr, row-1, col, size);
-  [arr, size] = fill(arr, row, col-1, size);
-  [arr, size] = fill(arr, row, col+1, size);
-  return [arr, size];
+  if(row < inpt.length-1)
+    size = fill(row+1, col, size);
+  if(row > 0)
+    size = fill(row-1, col, size);
+  if(col > 0)
+    size = fill(row, col-1, size);
+  if(col < inpt[0].length-1)
+    size = fill(row, col+1, size);
+  return size;
 }

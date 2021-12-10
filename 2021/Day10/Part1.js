a60cde237fbea7e86bde214cf8712b4f1698281c
Batophobia <godrox3@yahoo.com>
2021-12-10 08:50:37 -0600

    Update Part1.js

--- 2021/Day10/Part1.js
+++ 2021/Day10/Part1.js
@@ -1,5 +1,30 @@
+function checkLine(line){
+	expect=[];
+	for(c = 0; c < line.length; c++){
+    switch(line[c]){
+      case "(": expect.push(")"); break;
+      case "[": expect.push("]"); break;
+      case "{": expect.push("}"); break;
+      case "<": expect.push(">"); break;
+		  case expect[expect.length-1]: expect.pop(); break;
+      default: return line[c];
+    }
+  }
+  return 0;
+}
 function aoc(){
   inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  inpt = inpt.split(/\n/g).filter(v=>v);
+  ttl = 0;
+  for(i = 0; i < inpt.length; i++){
+    bad = checkLine(inpt[i]);
+    switch(bad){
+      case ")": ttl += 3; break;
+      case "]": ttl += 57; break;
+      case "}": ttl += 1197; break;
+      case ">": ttl += 25137; break;
+      default: break;
+    }
+  }
+  return ttl;
 }

7ee4700a65102618ecaf1d5537a8198c464df0a6
Batophobia <godrox3@yahoo.com>
2021-12-11 23:21:55 -0600

    Create Part1.js

--- 2021/Day12/Part1.js
+++ 2021/Day12/Part1.js
@@ -1,5 +1,14 @@
 function aoc(){
   inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  inpt = inpt.split(/\n/g).filter(v=>v);
+  paths = {};
+  for(i = 0; i< inpt.length; i++){
+    cave = inpt[i].split('-');
+    if(!paths[cave[0]]) paths[cave[0]] = { exits: [] };
+    if(!paths[cave[1]]) paths[cave[1]] = { exits: [] };
+    if(cave[0] != "end" && cave[1] != "start")
+      paths[cave[0]].exits.push(cave[1]);
+    if(cave[1] != "end" && cave[0] != "start")
+      paths[cave[1]].exits.push(cave[0]);
+  }
 }

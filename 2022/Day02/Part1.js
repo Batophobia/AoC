815e77d6270bef685aef2afd3966a5abe4270723
batophobia <godrox3@yahoo.com>
2022-12-02 12:12:36 -0600

    Day 2

--- 2022/Day02/Part1.js
+++ 2022/Day02/Part1.js
@@ -1,5 +1,30 @@
-function aoc(){
+function aoc() {
   inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  inpt = inpt.split(/\n/g);
+
+  ttl = 0;
+  for (i in inpt) {
+    if (!inpt[i]) continue;
+    opp = inpt[i].charCodeAt(0) - 65;
+    ply = inpt[i].charCodeAt(2) - 88;
+
+    ttl += ply + 1;
+
+    switch (ply) {
+      case 0: // Rock
+        if (opp == 0) ttl += 3;
+        else if (opp == 2) ttl += 6;
+        break;
+      case 1: // Paper
+        if (opp == 0) ttl += 6;
+        else if (opp == 1) ttl += 3;
+        break;
+      case 2: // Scissors
+        if (opp == 1) ttl += 6;
+        else if (opp == 2) ttl += 3;
+        break;
+    }
+  }
+
+  console.log(ttl);
 }
--- 2022/Day02/Part2.js
+++ 2022/Day02/Part2.js
@@ -1,5 +1,31 @@
-function aoc(){
+function aoc() {
   inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  inpt = inpt.split(/\n/g);
+
+  ttl = 0;
+  for (i in inpt) {
+    if (!inpt[i]) continue;
+    opp = inpt[i].charCodeAt(0) - 65;
+    ply = inpt[i].charCodeAt(2) - 88;
+
+    ttl += ply * 3;
+
+    switch (ply) {
+      case 0: // Lose
+        if (opp == 0) ttl += 3;
+        else if (opp == 1) ttl += 1;
+        else if (opp == 2) ttl += 2;
+        break;
+      case 1: // Tie
+        ttl += opp + 1;
+        break;
+      case 2: // Win
+        if (opp == 0) ttl += 2;
+        else if (opp == 1) ttl += 3;
+        else if (opp == 2) ttl += 1;
+        break;
+    }
+  }
+
+  console.log(ttl);
 }

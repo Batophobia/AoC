0ad8291d3ed0948f6cef60fff4fc2291cba9a0dd
batophobia <godrox3@yahoo.com>
2022-12-04 01:39:05 -0600

    Day 4

--- 2022/Day04/Part1.js
+++ 2022/Day04/Part1.js
@@ -2,5 +2,15 @@ function aoc() {
   inpt = document.body.textContent || document.body.innerText;
   inpt = inpt.split(/\n/g).filter(v => v);
 
+  ttl = 0;
+  for (i = 0; i < inpt.length; i++) {
+    elf = inpt[i].split(',');
+    elf[0] = elf[0].split('-').map(v => parseInt(v));
+    elf[1] = elf[1].split('-').map(v => parseInt(v));
 
+    if (elf[0][0] <= elf[1][0] && elf[0][1] >= elf[1][1]) ttl++;
+    else if (elf[1][0] <= elf[0][0] && elf[1][1] >= elf[0][1]) ttl++;
+  }
+
+  console.log(ttl);
 }
--- 2022/Day04/Part2.js
+++ 2022/Day04/Part2.js
@@ -1,5 +1,21 @@
-function aoc(){
+function aoc() {
   inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  inpt = inpt.split(/\n/g).filter(v => v);
+
+  ttl = 0;
+  for (i = 0; i < inpt.length; i++) {
+    elf = inpt[i].split(',');
+
+    elf[0] = elf[0].split('-').map(v => parseInt(v));
+    elf[0] = { min: elf[0][0], max: elf[0][1] };
+
+    elf[1] = elf[1].split('-').map(v => parseInt(v));
+    elf[1] = { min: elf[1][0], max: elf[1][1] };
+
+    elf.sort((x, y) => x.min - y.min);
+
+    if (elf[0].max >= elf[1].min) ttl++;
+  }
+
+  console.log(ttl);
 }

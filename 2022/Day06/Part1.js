2e24296d6472772e55ceafbafee95f40efa00853
batophobia <godrox3@yahoo.com>
2022-12-06 13:13:50 -0600

    Day 6

--- 2022/Day06/Part1.js
+++ 2022/Day06/Part1.js
@@ -1,5 +1,12 @@
-function aoc(){
+function aoc() {
   inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  inpt = inpt.split('');
+
+  idx = 0;
+  while (inpt.slice(idx, idx + 4)
+    .filter((v, i, s) => s.indexOf(v) === i).length < 4) {
+    idx++;
+  }
+
+  console.log(idx + 5);
 }
--- 2022/Day06/Part2.js
+++ 2022/Day06/Part2.js
@@ -1,5 +1,12 @@
-function aoc(){
+function aoc() {
   inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  inpt = inpt.split('');
+
+  idx = 0;
+  while (inpt.slice(idx, idx + 14)
+    .filter((v, i, s) => s.indexOf(v) === i).length < 14) {
+    idx++;
+  }
+
+  console.log(idx + 14);
 }

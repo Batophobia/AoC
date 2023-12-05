e06d35bb1ef779dd07ae983653c8b8fad3f5ddf0
batophobia <godrox3@yahoo.com>
2023-12-05 16:39:49 -0600

    Day 3

--- 2023/Day03/Part1.js
+++ 2023/Day03/Part1.js
@@ -1,5 +1,61 @@
 function aoc() {
-  inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  var inpt = document.body.textContent || document.body.innerText;
+  inpt = inpt.split(/\n/g);
+
+  var ttl = 0;
+  var regex = RegExp(/[^.\d]/);
+
+  for (var i = 0; i < inpt.length - 1; i++) {
+    var j = 0;
+    var num = '';
+    var startX = -1;
+
+    for (var j = 0; j < inpt[i].length; j++) {
+      if (!isNaN(inpt[i][j])) {
+        if (startX < 0)
+          startX = j;
+        num += inpt[i][j];
+      }
+
+      if (isNaN(inpt[i][j]) || j == inpt[i].length - 1) {
+        if (startX >= 0) {
+          var hasSymbol = false;
+          for (var x = startX - 1; x <= j; x++) {
+            if (x < 0 || x > inpt[i].length)
+              continue;
+
+            if (i > 0) {
+              // Check prev row
+              if (regex.test(inpt[i - 1][x])) {
+                hasSymbol = true;
+                break;
+              }
+            }
+
+            // Check this row
+            if (regex.test(inpt[i][x])) {
+              hasSymbol = true;
+              break;
+            }
+
+            if (i < inpt.length - 2) {
+              // Check next row
+              if (regex.test(inpt[i + 1][x])) {
+                hasSymbol = true;
+                break;
+              }
+            }
+          }
+
+          if (hasSymbol) {
+            ttl += parseInt(num);
+          }
+        }
+        num = '';
+        startX = -1;
+      }
+    }
+  }
+
+  console.log(ttl);
 }
--- 2023/Day03/Part2.js
+++ 2023/Day03/Part2.js
@@ -1,5 +1,92 @@
+function getNumber(x, row) {
+  while (x > 0 && !isNaN(row[x])) {
+    x--;
+  }
+
+  if (isNaN(row[x])) x++;
+
+  var num = '';
+  while (x < row.length && !isNaN(row[x])) {
+    num += row[x];
+    x++;
+  }
+
+  return num;
+}
+
 function aoc() {
-  inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  var inpt = document.body.textContent || document.body.innerText;
+  inpt = inpt.split(/\n/g);
+
+  var ttl = 0;
+
+  for (var i = 0; i < inpt.length - 1; i++) {
+    var j = 0;
+
+    for (var j = 0; j < inpt[i].length; j++) {
+      if (inpt[i][j] == '*') {
+        var nums = [];
+        var temp = -1;
+
+        // Check up
+        if (i > 0) {
+          if (!isNaN(inpt[i - 1][j])) {
+            temp = getNumber(j, inpt[i - 1]);
+            if (temp != '')
+              nums.push(temp);
+          } else {
+            if (j > 0 && !isNaN(inpt[i - 1][j - 1])) {
+              temp = getNumber(j - 1, inpt[i - 1]);
+              if (temp != '')
+                nums.push(temp);
+            }
+            if (j < inpt[i].length - 1 && !isNaN(inpt[i - 1][j + 1])) {
+              temp = getNumber(j + 1, inpt[i - 1]);
+              if (temp != '')
+                nums.push(temp);
+            }
+          }
+        }
+
+        // Check down
+        if (i < inpt.length - 2) {
+          if (!isNaN(inpt[i + 1][j])) {
+            temp = getNumber(j, inpt[i + 1]);
+            if (temp != '')
+              nums.push(temp);
+          } else {
+            if (j > 0 && !isNaN(inpt[i + 1][j - 1])) {
+              temp = getNumber(j - 1, inpt[i + 1]);
+              if (temp != '')
+                nums.push(temp);
+            }
+            if (j < inpt[i].length - 1 && !isNaN(inpt[i + 1][j + 1])) {
+              temp = getNumber(j + 1, inpt[i + 1]);
+              if (temp != '')
+                nums.push(temp);
+            }
+          }
+        }
+
+        // Check left
+        if (j > 0 && !isNaN(inpt[i][j - 1])) {
+          temp = getNumber(j - 1, inpt[i]);
+          if (temp != '')
+            nums.push(temp);
+        }
+
+        // Check right
+        if (j < inpt[i].length - 1 && !isNaN(inpt[i][j + 1])) {
+          temp = getNumber(j + 1, inpt[i]);
+          if (temp != '')
+            nums.push(temp);
+        }
+
+        if (nums.length == 2) {
+          ttl += nums[0] * nums[1];
+        }
+      }
+    }
+  }
+  console.log(ttl);
 }

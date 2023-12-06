cfa1823b7b4b59944c5cdec1055b9200939b3f0f
batophobia <godrox3@yahoo.com>
2023-12-06 10:52:24 -0600

    Day 4

--- 2023/Day04/Part1.js
+++ 2023/Day04/Part1.js
@@ -1,5 +1,24 @@
 function aoc() {
-  inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  var inpt = document.body.textContent || document.body.innerText;
+  inpt = inpt.split(/\n/g);
+
+  var ttl = 0;
+
+  for (var i = 0; i < inpt.length - 1; i++) {
+    var [wins, nums] = inpt[i].substring(inpt[i].indexOf(':') + 1).split('|');
+    wins = wins.trim().split(/\s+/g);
+    nums = nums.trim().split(/\s+/g);
+
+    var points = 0.5;
+    for (var n = 0; n < nums.length; n++) {
+      for (var w = 0; w < wins.length; w++) {
+        if (nums[n] == wins[w]) {
+          points *= 2;
+        }
+      }
+    }
+
+    ttl += Math.floor(points);
+  }
+  console.log(ttl);
 }
--- 2023/Day04/Part2.js
+++ 2023/Day04/Part2.js
@@ -1,5 +1,31 @@
 function aoc() {
-  inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  var inpt = document.body.textContent || document.body.innerText;
+  inpt = inpt.split(/\n/g);
+
+  var ttl = 0;
+  var copies = Array(inpt.length - 1).fill(1);
+
+  for (var i = 0; i < inpt.length - 1; i++) {
+    var [wins, nums] = inpt[i].substring(inpt[i].indexOf(':') + 1).split('|');
+    wins = wins.trim().split(/\s+/g);
+    nums = nums.trim().split(/\s+/g);
+
+    var matches = 0;
+
+    for (var n = 0; n < nums.length; n++) {
+      for (var w = 0; w < wins.length; w++) {
+        if (nums[n] == wins[w]) {
+          matches++;
+        }
+      }
+    }
+
+    for (var c = 1; c <= matches; c++) {
+      copies[i + c] += copies[i];
+    }
+
+    ttl += copies[i];
+  }
+
+  console.log(ttl);
 }

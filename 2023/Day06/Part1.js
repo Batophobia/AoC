33341bec9178c6f44df26644debe6e3b280e79d9
batophobia <godrox3@yahoo.com>
2023-12-06 21:01:17 -0600

    Day 6

--- 2023/Day06/Part1.js
+++ 2023/Day06/Part1.js
@@ -1,5 +1,25 @@
 function aoc() {
-  inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  var inpt = (document.body.textContent || document.body.innerText).trim();
+  inpt = inpt.split(/\n/g);
+
+  var times = inpt[0].split(/\s+/g);
+  var distances = inpt[1].split(/\s+/g);
+
+  var ttl = 1;
+
+  for (var i = 1; i < times.length; i++) {
+    var curTime = times[i];
+    var curDist = distances[i];
+    var numWins = 0;
+
+    for (var t = curTime - 1; t > 0; t--) {
+      var dist = t * (curTime - t);
+      if (dist > curDist)
+        numWins++;
+    }
+
+    ttl *= numWins;
+  }
+
+  console.log(ttl);
 }
--- 2023/Day06/Part2.js
+++ 2023/Day06/Part2.js
@@ -1,5 +1,17 @@
 function aoc() {
-  inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+  var inpt = (document.body.textContent || document.body.innerText).trim();
+  inpt = inpt.split(/\n/g);
+
+  var time = parseInt(inpt[0].split(':')[1].replace(/\s/g, ''));
+  var distance = parseInt(inpt[1].split(':')[1].replace(/\s/g, ''));
+
+  var ttl = 0;
+
+  for (var t = time - 1; t > 0; t--) {
+    var dist = t * (time - t);
+    if (dist > distance)
+      ttl++;
+  }
+
+  console.log(ttl);
 }

81c857116992944a61089289cabb743bfb0ac0df
batophobia <godrox3@yahoo.com>
2023-12-08 16:17:05 -0600

    Day 8

--- 2023/Day08/Part1.js
+++ 2023/Day08/Part1.js
@@ -1,5 +1,23 @@
 function aoc() {
-  inpt = document.body.textContent || document.body.innerText;
+  var inpt = (document.body.textContent || document.body.innerText).trim();
   inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+
+  var dir = inpt[0].replaceAll('L', '0').replaceAll('R', '1');
+  var locs = inpt[1].split(/\n/g);
+  var curLoc = 'AAA';
+  var dirIdx = 0;
+  var locMap = {};
+
+  for (var i = 0; i < locs.length; i++) {
+    var key = locs[i].split('=')[0].trim();
+    var [left, right] = locs[i].split('=')[1].trim().replace(/[()\s]/g, '').split(',');
+    locMap[key] = [left, right];
+  }
+
+  while (curLoc != 'ZZZ' && dirIdx < 99999) {
+    curLoc = locMap[curLoc][dir[dirIdx % dir.length]]
+    dirIdx++;
+  }
+
+  console.log(dirIdx);
 }
--- 2023/Day08/Part2.js
+++ 2023/Day08/Part2.js
@@ -1,5 +1,50 @@
-function aoc(){
-  inpt = document.body.textContent || document.body.innerText;
+function aoc() {
+  var inpt = (document.body.textContent || document.body.innerText).trim();
   inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+
+  var dir = inpt[0].replaceAll('L', '0').replaceAll('R', '1');
+  var locs = inpt[1].split(/\n/g);
+  var curLocs = {};
+  var locMap = {};
+
+  for (var i = 0; i < locs.length; i++) {
+    var key = locs[i].split('=')[0].trim();
+    var [left, right] = locs[i].split('=')[1].trim().replace(/[()\s]/g, '').split(',');
+    locMap[key] = [left, right];
+
+    if (key[key.length - 1] == 'A')
+      curLocs[key] = -1;
+  }
+
+  for (var loc in curLocs) {
+    var curLoc = loc;
+    var inMaze = true;
+    var dirIdx = 0;
+
+    while (inMaze && dirIdx < 99999999999) {
+      inMaze = false;
+
+      if (locMap[curLoc].length > 2) {
+        dirIdx += locMap[curLoc][2];
+      }
+      curLoc = locMap[curLoc][dir[dirIdx % dir.length]]
+
+      if (curLoc[curLoc.length - 1] != 'Z')
+        inMaze = true;
+
+      dirIdx++;
+    }
+
+    curLocs[loc] = dirIdx;
+  }
+
+  var nums = Object.values(curLocs);
+  nums = nums.reduce((x, y) => (x * y) / gcd(x, y));
+  console.log(nums);
+}
+
+function gcd(a, b) {
+  if (b === 0) return a;
+
+  return gcd(b, a % b);
 }

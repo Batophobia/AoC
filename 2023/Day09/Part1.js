eaab8a2a4c12750db6273d917d046c57286bae13
batophobia <godrox3@yahoo.com>
2023-12-10 17:54:54 -0600

    Day 9

--- 2023/Day09/Part1.js
+++ 2023/Day09/Part1.js
@@ -1,5 +1,28 @@
-function aoc(){
-  inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+function aoc() {
+  var inpt = (document.body.textContent || document.body.innerText).trim();
+  inpt = inpt.split(/\n/g);
+
+  var ttl = 0;
+
+  for (var i = 0; i < inpt.length; i++) {
+    var curLine = inpt[i].split(' ').map(Number);
+
+    var maxDiffs = [curLine[curLine.length - 1]];
+    var isDiff = true;
+    while (isDiff) {
+      var changes = [];
+      for (var j = 1; j < curLine.length; j++) {
+        changes.push(curLine[j] - curLine[j - 1]);
+      }
+      maxDiffs.push(changes[changes.length - 1]);
+
+      isDiff = !changes.every((val, x, arr) => val === arr[0]);
+
+      curLine = changes;
+    }
+
+    ttl += maxDiffs.reduce((t, v) => t + v, 0);
+  }
+
+  console.log(ttl);
 }
--- 2023/Day09/Part2.js
+++ 2023/Day09/Part2.js
@@ -1,5 +1,30 @@
-function aoc(){
-  inpt = document.body.textContent || document.body.innerText;
-  inpt = inpt.split(/\n\n/g);
-  // Do Stuff
+function aoc() {
+  var inpt = (document.body.textContent || document.body.innerText).trim();
+  inpt = inpt.split(/\n/g);
+
+  var ttl = 0;
+
+  for (var i = 0; i < inpt.length; i++) {
+    var curLine = inpt[i].split(' ').map(Number);
+
+    var minDiffs = [curLine[0]];
+    var isDiff = true;
+    while (isDiff) {
+      var changes = [];
+      for (var j = 1; j < curLine.length; j++) {
+        changes.push(curLine[j] - curLine[j - 1]);
+      }
+      minDiffs.push(changes[0]);
+
+      isDiff = !changes.every((val, x, arr) => val === arr[0]);
+
+      curLine = changes;
+    }
+
+    for (var j in minDiffs) {
+      ttl += minDiffs[j] * (j % 2 > 0 ? -1 : 1);
+    }
+  }
+
+  console.log(ttl);
 }

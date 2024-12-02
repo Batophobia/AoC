370f4a6b814528b45cede83d1bb2fbbab23d3e75
batophobia <godrox3@yahoo.com>
2024-12-01 23:30:20 -0600

    Day 2

--- 2024/Day02/Part1.js
+++ 2024/Day02/Part1.js
@@ -3,7 +3,31 @@ function aoc() {
   inpt = inpt.split(/\n/g);
   ttl = 0;
 
-  // TODO
+  for (i = 0; i < inpt.length; i++) {
+    nums = inpt[i].split(/\s+/g);
+    prvNum = parseInt(nums[0]);
+    dir = 0;
+    isSafe = true;
+
+    for (j = 1; j < nums.length; j++) {
+      curNum = parseInt(nums[j]);
+      if ((prvNum < curNum && dir < 0)
+        || (prvNum > curNum && dir > 0)
+        || Math.abs(prvNum - curNum) > 3
+        || Math.abs(prvNum - curNum) < 1
+      ) {
+        isSafe = false;
+        break;
+      }
+
+      if (j == 1) {
+        dir = prvNum > curNum ? -1 : 1
+      }
+      prvNum = curNum
+    }
+
+    if (isSafe) ttl++;
+  }
 
   console.log(ttl);
 }
--- 2024/Day02/Part2.js
+++ 2024/Day02/Part2.js
@@ -1,11 +1,49 @@
 function aoc() {
-  inpt = (document.body.textContent || document.body.innerText).trim()
+  let inpt = (document.body.textContent || document.body.innerText).trim()
   inpt = inpt.split(/\n/g);
-  ttl = 0;
+  let ttl = 0;
 
-  // TODO
+  for (i = 0; i < inpt.length; i++) {
+    let nums = inpt[i].split(/\s+/g);
+
+    let errors = checkSafe(nums);
+
+    if (errors > 0) {
+      for (x = 0; x < nums.length; x++) {
+        errors = checkSafe(nums.filter((v, i) => i != x))
+        if (errors < 1) break;
+      }
+    }
+
+    if (errors < 1) ttl++;
+  }
 
   console.log(ttl);
+}
+
+function checkSafe(arr) {
+  let prvNum = parseInt(arr[0]);
+  let dir = 0;
+  let errors = 0;
+
+  for (j = 1; j < arr.length; j++) {
+    curNum = parseInt(arr[j]);
+    if ((prvNum < curNum && dir < 0)
+      || (prvNum > curNum && dir > 0)
+      || Math.abs(prvNum - curNum) > 3
+      || Math.abs(prvNum - curNum) < 1
+    ) {
+      errors++;
+      continue;
+    }
+
+    if (j == 1) {
+      dir = prvNum > curNum ? -1 : 1
+    }
+    prvNum = curNum
+  }
+
+  return errors
 }
 
 aoc()
